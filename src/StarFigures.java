b76a56cffb8d94fc22a57f6b806dcb00977195a3
Brian Williams <bwilliams8@live.maryville.edu>
2018-03-18 23:08:19 -0500

    Completed Assignment

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -1,16 +1,51 @@
 /*
   	ISYS 320
-  	Name(s):
+  	Name(s): Brian Williams
   	Date: 
 */
 
 // 4. Your pseudocode algorithm for how to break down the figure
+/*
+Three base variables:
+*****
+ * *
+  *
+
+Beyond this, seperate methods to call them in different orders.
+
+ */
 
 
 public class StarFigures {
 
+	static String fiveStar = "*****";
+	static String twoStar = " * * ";
+	static String oneStar = "  *  ";
+	static String milhouseString = "Milhouse did not test cootie positive.";
+	
 	public static void main(String[] args) {
-
+		starDesignOne();
+		starDesignTwo();
+		starDesignThree();
+		milhouse();
 	}
+	
+    public static void starDesignOne() {
+        System.out.println(fiveStar + '\n' + fiveStar + '\n' + twoStar + '\n' + oneStar + '\n' + twoStar);
+    }
+    
+    public static void starDesignTwo() {
+    	System.out.println(fiveStar +'\n' + fiveStar + '\n' + twoStar + '\n' + oneStar + '\n' + twoStar + '\n' + fiveStar + '\n' + fiveStar);
+    }
+    
+    public static void starDesignThree() {
+    	System.out.println(oneStar + '\n' + oneStar + '\n' + oneStar + '\n' + fiveStar + '\n' + fiveStar + '\n' + twoStar + '\n' + oneStar + '\n' + twoStar);
+    }
+	
+    public static void milhouse() {
+    	String milhouse1000;
+    	milhouse1000 = new String(new char[1000]).replace("\0", milhouseString + '\n');
+    	System.out.println(milhouse1000);
+    }
 
 }
--- src/Tricky.java
+++ src/Tricky.java
@@ -1,17 +1,46 @@
 /*
   	ISYS 320
-  	Name(s):
+  	Name(s): Brian Williams
   	Date: 
 */
 
 // 1. Your predicted output 
+
+// Based mostly on my swift knowledge:
+// message1 and message2 are functions / methods. message1 prints "This is message1" to console then returns, while message2 prints "This is message2.", followed by calling message1, then printing "Done with message 2."
+// The console output would be:
+/*
+This is message1.
+This is message2.
+This is message1.
+Done with message 2.
+Done with main.
+*/
  
 
 public class Tricky {
+	    public static void main(String[] args) {
+	        message1();
+	        message2();
+	        System.out.println("Done with main.");
+	    }
+	    
+	    public static void message1() {
+	        System.out.println("This is message1.");
+	    }
+	    
+	    public static void message2() {
+	        System.out.println("This is message2.");
+	        message1();
+	        System.out.println("Done with message 2.");
+	    }
+
 
 }
 
 // 3. Were you correct? Explain any differences
+
+// I was correct.
  
  
 
